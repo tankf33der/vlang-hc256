f656e3e3305b75e0fc60ef7c2b702e847954007c
Mike <tankf33der@disroot.org>
2025-03-02 13:37:12 +0200

    .

--- hc256/hc256.v
+++ hc256/hc256.v
@@ -2,7 +2,7 @@ module hc256
 
 import math.bits
 
-pub struct Hc256 {
+pub struct Hc256 {                  // XXX
 pub mut:
     p               [1024]u32
     q               [1024]u32
@@ -11,36 +11,38 @@ pub mut:
     counter2048     u32
 }
 
-// Helpers for initialization()
+fn rotr (x u32, k int) u32 {
+    return (x >> k) | (x << (32 - k))
+}
 
-@[inline]
+// Helpers for initialization()
 fn f1 (x u32) u32 {
     return bits.rotate_left_32(x, -7) ^ bits.rotate_left_32(x, -18) ^ (x >> 3)
 }
 
-@[inline]
 fn f2 (x u32) u32 {
     return bits.rotate_left_32(x, -17) ^ bits.rotate_left_32(x, -19) ^ (x >> 10)
 }
 
-@[inline]
 fn f (a u32, b u32, c u32, d u32) u32 {
-    return f2(a) + b + d
+    return f2(a) + b + f1(c) + d
 }
 
-@[inline]
 fn (mut h Hc256) feedback_1(mut u[] u32, v u32, b u32, c u32) {
     mut temp0 := u32(0)
     mut temp1 := u32(0)
     mut temp2 := u32(0)
 
+    println('b:${u[0]}')
     temp0 = bits.rotate_left_32(v, -23)
     temp1 = bits.rotate_left_32(c, -10)
     temp2 = (v ^ c) & 0x3ff
-    u[0] += b + (temp0 ^ temp1) + h.q[temp2]
+    println('${temp0}:${temp1}:${temp2}')
+    // u[0] += b + (temp0 ^ temp1) + h.q[temp2]
+    u[0] += h.q[temp2]
+    println('a:${u[0]}')
 }
 
-@[inline]
 fn (mut h Hc256) feedback_2(mut u[] u32, v u32, b u32, c u32) {
     mut temp0 := u32(0)
     mut temp1 := u32(0)
@@ -54,7 +56,6 @@ fn (mut h Hc256) feedback_2(mut u[] u32, v u32, b u32, c u32) {
 
 // Helpers for encrypt()
 
-@[inline]
 fn (mut h Hc256) h1(x u32, mut y &u32) {
     mut a := u8(0)
     mut b := u8(0)
@@ -68,7 +69,6 @@ fn (mut h Hc256) h1(x u32, mut y &u32) {
     y = h.q[a]+h.q[256+b]+h.q[512+c]+h.q[768+d]
 }
 
-@[inline]
 fn (mut h Hc256) h2(x u32, mut y &u32) {
     mut a := u8(0)
     mut b := u8(0)
@@ -82,7 +82,6 @@ fn (mut h Hc256) h2(x u32, mut y &u32) {
     y = h.p[a]+h.p[256+b]+h.p[512+c]+h.p[768+d]
 }
 
-@[inline]
 fn (mut h Hc256) step_a(mut u[] u32, v u32, mut a[] u32, b u32, c u32, d u32, mut m[] u32) {
     mut temp0 := u32(0)
     mut temp1 := u32(0)
@@ -98,7 +97,6 @@ fn (mut h Hc256) step_a(mut u[] u32, v u32, mut a[] u32, b u32, c u32, d u32, mu
     m[0] ^= temp3 ^ u[0]
 }
 
-@[inline]
 fn (mut h Hc256) step_b(mut u[] u32, v u32, mut a[] u32, b u32, c u32, d u32, mut m[] u32) {
     mut temp0 := u32(0)
     mut temp1 := u32(0)
@@ -114,7 +112,6 @@ fn (mut h Hc256) step_b(mut u[] u32, v u32, mut a[] u32, b u32, c u32, d u32, mu
     m[0] ^= temp3 ^ u[0]
 }
 
-
 pub fn (mut h Hc256) initialization(key []u32, iv []u32) {
     for i := 0; i < 8;  i++ { h.p[i] = key[i] }
     for i := 8; i < 16; i++ { h.p[i] =  iv[i-8] }
@@ -122,50 +119,47 @@ pub fn (mut h Hc256) initialization(key []u32, iv []u32) {
     for i := u32(16); i < 528; i++ {
         h.p[i] = f(h.p[i-2],h.p[i-7],h.p[i-15],h.p[i-16])+i
     }
-    println(h.p)
-    println('${h.p[500]}, ${h.q[500]}')
-
     for i := 0; i < 16; i++ {
         h.p[i] = h.p[i+512]
     }
-    // for i := u32(16); i < 1024; i++ {
-    //     h.p[i] = f(h.p[i-2],h.p[i-7],h.p[i-15],h.p[i-16])+512+i
-    // }
-//
-//     for i := 0; i < 16; i++ {
-//         h.q[i] = h.p[1024-16+i]
-//     }
-//     for i := u32(16); i < 32; i++ {
-//         h.q[i] = f(h.q[i-2],h.q[i-7],h.q[i-15],h.q[i-16])+1520+i
-//     }
-//     for i := 0; i < 16; i++ {
-//         h.q[i] = h.q[i+16]
-//     }
-//     for i := u32(16); i < 1024; i++ {
-//         h.q[i] = f(h.q[i-2],h.q[i-7],h.q[i-15],h.q[i-16])+1536+i
-//     }
+    for i := u32(16); i < 1024; i++ {
+        h.p[i] = f(h.p[i-2],h.p[i-7],h.p[i-15],h.p[i-16])+512+i
+    }
+
+    for i := 0; i < 16; i++ {
+        h.q[i] = h.p[1024-16+i]
+    }
+    for i := u32(16); i < 32; i++ {
+        h.q[i] = f(h.q[i-2],h.q[i-7],h.q[i-15],h.q[i-16])+1520+i
+    }
+    for i := 0; i < 16; i++ {
+        h.q[i] = h.q[i+16]
+    }
+    for i := u32(16); i < 1024; i++ {
+        h.q[i] = f(h.q[i-2],h.q[i-7],h.q[i-15],h.q[i-16])+1536+i
+    }
+
 
     //run the cipher 4096 steps without generating output
-//     for i := 0; i < 2; i++ {
-//         for j := 0; j < 10; j++ {
-//             h.feedback_1(mut h.p[j..],h.p[j+1],h.p[(j-10)&0x3ff],h.p[(j-3)&0x3ff])
-//         }
-//         for j := 10; j < 1023; j++ {
-//             t := int(1023)
-//
-//             h.feedback_1(mut h.p[j..],h.p[j+1],h.p[j-10],h.p[j-3])
-//             h.feedback_1(mut h.p[t..],h.p[0],h.p[1013],h.p[1020])
-//         }
-//         for j := 0; j < 10; j++ {
-//             h.feedback_2(mut h.q[j..],h.q[j+1],h.q[(j-10)&0x3ff],h.q[(j-3)&0x3ff])
-//         }
-//         for j := 10; j < 1023; j++ {
-//             t := int(1023)
-//
-//             h.feedback_2(mut h.q[j..],h.q[j+1],h.q[j-10],h.q[j-3])
-//             h.feedback_2(mut h.q[t..],h.q[0],h.q[1013],h.q[1020])
-//         }
-//     }
+    for i := 0; i < 2; i++ {
+        for j := 0; j < 10; j++ {
+            h.feedback_1(mut h.p[j..],h.p[j+1],h.p[(j-10)&0x3ff],h.p[(j-3)&0x3ff])
+        }
+/*
+        for j := 10; j < 1023; j++ {
+            h.feedback_1(mut h.p[j..],h.p[j+1],h.p[j-10],h.p[j-3])
+        }
+        h.feedback_1(mut h.p[1023..],h.p[0],h.p[1013],h.p[1020])
+
+        for j := 0; j < 10; j++ {
+            h.feedback_2(mut h.q[j..],h.q[j+1],h.q[(j-10)&0x3ff],h.q[(j-3)&0x3ff])
+        }
+        for j := 10; j < 1023; j++ {
+            h.feedback_2(mut h.q[j..],h.q[j+1],h.q[j-10],h.q[j-3])
+        }
+        h.feedback_2(mut h.q[1023..],h.q[0],h.q[1013],h.q[1020])
+*/
+    }
 
     h.counter2048 = 0
     for i := 0; i < 16; i++ { h.x[i] = h.p[1008+i] }
