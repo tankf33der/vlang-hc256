90c9f730e167f80256b11098cf248089ac940985
Mike <tankf33der@disroot.org>
2025-03-16 16:37:44 +0200

    .

--- vectors_test.v
+++ vectors_test.v
@@ -11,18 +11,18 @@ fn test_vectors() {
 		h.u32()!
 	}
 	assert h.u32()! == 2171174450
-	h.free()
+	unsafe { h.free() }
 
 	h = hc256.Hc256{}
 	iv[0] = 1
 	h.seed(key, iv)
 	assert h.u32()! == 3215123119
-	h.free()
+	unsafe { h.free() }
 
 	h = hc256.Hc256{}
 	iv[0] = 0
 	key[0] = 0x55
 	h.seed(key, iv)
 	assert h.u32()! == 4266278940
-	h.free()
+	unsafe { h.free() }
 }
